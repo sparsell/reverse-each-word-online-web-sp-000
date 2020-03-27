55435aab320c11abe6f0f85935eeb75c06fe201f
Sonja Parsell <hello@sparsell.com>
2020-03-27 13:48:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   reverse_sentence_array = []
-  sentence_array.each do |word|
+  sentence_array.collect do |word|
   reverse_sentence_array << word.reverse
   end
    reverse_sentence_array.join(" ")
