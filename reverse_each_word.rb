fab06914bd1b93c18b23652d5e7c820eea5bdb88
Sonja Parsell <hello@sparsell.com>
2020-03-27 09:58:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,8 @@
 #returns that same sentence with each word reversed in place.
 
 def reverse_each_word(sentence)
-sentence.split(' ')
-sentence.each do |word|
+  sentence.split(' ')
+  sentence.each do |word|
   puts sentence
   end
 end
